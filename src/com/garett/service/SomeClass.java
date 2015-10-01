3d78ef53a60ee7ee02e92f915e7e4caa507a4f31
GBynum <garett.a.bynum@ehi.com>
2015-10-01 14:00:03 -0500

    added string and integer to SomeClass

--- src/com/garett/service/SomeClass.java
+++ src/com/garett/service/SomeClass.java
@@ -2,4 +2,22 @@ package com.garett.service;
 
 public class SomeClass {
 
+	private String string;
+	private Integer integer;
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	public Integer getInteger() {
+		return integer;
+	}
+
+	public void setInteger(Integer integer) {
+		this.integer = integer;
+	}
 }
