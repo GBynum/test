8262fb37eae291434207337faef60a671fd204d7
GBynum <garett.a.bynum@ehi.com>
2015-10-01 14:03:55 -0500

    added boolean and constructor to SomeClass

--- src/com/garett/service/SomeClass.java
+++ src/com/garett/service/SomeClass.java
@@ -1,5 +1,20 @@
 package com.garett.service;
 
 public class SomeClass {
+	
+	private Boolean bool;
+
+	public SomeClass(Boolean bool) {
+		super();
+		this.bool = bool;
+	}
+
+	public Boolean getBool() {
+		return bool;
+	}
+
+	public void setBool(Boolean bool) {
+		this.bool = bool;
+	}
 
 }
